77cfce7302827b167242974863d94d4db7d3e358
Marius Schneider <156344021+MariusSchneider-0405@users.noreply.github.com>
2025-10-10 13:41:26 +0200

    Exercises 0 to 3 2025_10_10

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -10,9 +10,45 @@ int main(int argc, char **argv) {
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+    fmt::print("Hello, {}!\n", tea::PROJECT_NAME, tea::PROJECT_VER);
+
+//Teil 0:
+    for ( int i = 0; i < argc; i++) {
+        fmt::println("argc: {}, argument: {}, (Adresse: {})", i, argv[i], fmt::ptr(argv[i]));
+    }
 
     /* INSERT YOUR CODE HERE */
+//Teil 1:
+    int foo = 10;
 
-    return 0; /* exit gracefully*/
+    int* ptr = &foo;
+
+    *ptr = 42;
+
+    int bar = 4711;
+    ptr = &bar;
+    *ptr = foo;
+    fmt::print("bar = {}", bar) ;
+
+//Teil 2:
+    fmt::println("\n ----------------Teil 2: Speicherreservierung --------------------");
+    double* d = new double(3.1415);   // Speicher reservieren UND initialisieren
+    fmt::println("\nWert: {}, Adresse: {}", *d, fmt::ptr(d) );    // Zugriff über Dereferenzierung
+    delete d;                         // Speicher freigeben
+
+
+
+    return 0; /*exit gratefully*/
 }
+
+//Teil 3:
+
+void changeValueByParameter(int value) {
+    value = 4711;
+    fmt::println("Der Wert von Value in der Funktion (ChangeValuePyParameter): {}", value);
+    }
+void changeValueByPointer(int* ptr) {
+    *ptr = 4711;
+    fmt::println("Der wert von *ptr in der Funktion (changeValueByPointer): {}", *ptr);
+    fmt::println("Die Adresse von ptr in der Funtkion (changeValueByPointer): {}", fmt::ptr(ptr));
+    }
