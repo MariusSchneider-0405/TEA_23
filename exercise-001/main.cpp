8dde566aeabada5c3b2177a4cf0acc1ed80d9647
Marius Schneider <156344021+MariusSchneider-0405@users.noreply.github.com>
2025-10-17 14:42:53 +0200

    Exercise-001: Teil 1 Ergänzung

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -6,6 +6,10 @@
 int globalVar = 1;
 int Geburtstag = 4052004;
 int Alter = 21; 
+static int aaa = 1212;
+static int bbb = 3343;
+const int ccc = 120;
+const int ddd = 815;
 
 int main(int argc, char **argv) {
 
@@ -14,9 +18,16 @@ int main(int argc, char **argv) {
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
+    int xyz = 1234; 
+
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
     fmt::print("Wert: {}, \nAdresse: {}", globalVar, fmt::ptr(&globalVar) );
     fmt::print("\nGeburtstag: {}, \nAlter: {}, \nAdresse Geburtstag: {}, \nAdresse Alter: {}", Geburtstag, Alter, fmt::ptr(&Geburtstag), fmt::ptr(&Alter));
+    fmt::print("\nWert: {}, Adresse: {}", aaa, fmt::ptr(&aaa));
+    fmt::print("\nWert: {}, Adresse: {}", bbb, fmt::ptr(&bbb));
+    fmt::print("\nWert: {}, Adresse: {}", ccc, fmt::ptr(&ccc));
+    fmt::print("\nWert: {}, Adresse: {}", ddd, fmt::ptr(&ddd));
+    fmt::print("\nWert: {}, Adresse: {}", xyz, fmt::ptr(&xyz));
     /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
