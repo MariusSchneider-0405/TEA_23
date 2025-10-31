46d1aa7d23781317429c7924d6338087e3c37517
Marius Schneider <156344021+MariusSchneider-0405@users.noreply.github.com>
2025-10-31 15:39:31 +0100

    add Aufgabe 2 und 4

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,86 +1,122 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
-/* .bss Segment */
-int foo;
-int foo2 = 0;
+/*Aufgabe 2*/
+/*Beispiel 1 */
+#include <cstdlib>
+#include <cstring>
 
-/* .data Segment */
-int data = 45;
-int data2 = 43;
+// #define SOULS 100
 
-/* .rodata Segment */
-const int rodata = 45;
-const int rodata2 = 43;
+// void cursed() {
+//     char *soul = (char*)malloc(8);
+//     strcpy(soul, "boo!");
+//     fmt::println("{}", soul);
+//     free(soul);
+// }
 
+// int main() {
+//     for(int i=0; i < SOULS; i++) {
+//         cursed();
+//     }
+// }
 
+/*Beispiel 2*/
+// int main() {
+//     char *ghost = (char*)malloc(8);
+//     strcpy(ghost, "evil");
+//     free(ghost);
+//     fmt::println("{}", ghost);
+//     ghost = NULL;
+// }
 
-void doSomething(int value)
-{
-    value = value + 1;
-    fmt::print("doSomething, value {}\n", value);
+/*Beispiel 3*/
+int main() {
+    while (1) {
+        malloc(1024);
+    }
 }
 
-void doSomethingPtr(int* ptr)
-{
-    *ptr = *ptr + 1;
-    fmt::print("doSomethingPtr, value {}\n", *ptr);
-}
+// /* .bss Segment */
+// int foo;
+// int foo2 = 0;
 
-int* doReturnAPtr()
-{
-    int value = 4711;
-    fmt::print("doReturnAPtr, value {}\n", value);
-    return &value;
-}
+// /* .data Segment */
+// int data = 45;
+// int data2 = 43;
 
-void doSomethingStatic()
-{
-    static int value = 56;
-    value = value + 1;
-    fmt::print("doSomethingStatic, value {}\n", value);
-}
-auto main(int argc, char** argv) -> int
-{
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", argv[0]);
+// /* .rodata Segment */
+// const int rodata = 45;
+// const int rodata2 = 43;
 
-    doSomething(42);
-    int bar = 42;
-    doSomething(bar);
-    fmt::print("The value of bar is: {}\n", bar);
 
-    doSomethingPtr(&bar);
-    fmt::print("The value of bar after doSomethingPtr is: {}\n", bar);
 
-    int* returnedPtr = doReturnAPtr();
-    fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
-    doSomething(42);
-    fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
+// void doSomething(int value)
+// {
+//     value = value + 1;
+//     fmt::print("doSomething, value {}\n", value);
+// }
 
-    doSomethingStatic();
-    doSomethingStatic();
-    doSomethingStatic();
-    doSomethingStatic();
+// void doSomethingPtr(int* ptr)
+// {
+//     *ptr = *ptr + 1;
+//     fmt::print("doSomethingPtr, value {}\n", *ptr);
+// }
 
-    fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
-    fmt::print("Value of foo2 {} address of foo2 {}\n", foo2, fmt::ptr(&foo2));
-    fmt::print("Value of data {} address of data {}\n", data, fmt::ptr(&data));
-    fmt::print("Value of data2 {} address of data2 {}\n", data2, fmt::ptr(&data2));
-    fmt::print("Value of rodata {} address of rodata {}\n", rodata, fmt::ptr(&rodata));
-    fmt::print("Value of rodata2 {} address of rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
+// int* doReturnAPtr()
+// {
+//     int value = 4711;
+//     fmt::print("doReturnAPtr, value {}\n", value);
+//     return &value;
+// }
 
-    int var;
-    static int var2;
-    fmt::print("Value of var {} address of var {}\n", var, fmt::ptr(&var));
-    fmt::print("Value of var2 {} address of var2 {}\n", var2, fmt::ptr(&var2));
+// void doSomethingStatic()
+// {
+//     static int value = 56;
+//     value = value + 1;
+//     fmt::print("doSomethingStatic, value {}\n", value);
+// }
+// auto main(int argc, char** argv) -> int
+// {
+//     /**
+//      * The {fmt} lib is a cross platform library for printing and formatting text
+//      * it is much more convenient than std::cout and printf
+//      * More info at https://fmt.dev/latest/api.html
+//      */
+//     fmt::print("Hello, {}!\n", argv[0]);
 
-    return 0; /* exit gracefully*/
-}
+//     doSomething(42);
+//     int bar = 42;
+//     doSomething(bar);
+//     fmt::print("The value of bar is: {}\n", bar);
+
+//     doSomethingPtr(&bar);
+//     fmt::print("The value of bar after doSomethingPtr is: {}\n", bar);
+
+//     int* returnedPtr = doReturnAPtr();
+//     fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
+//     doSomething(42);
+//     fmt::print("The value of returnedPtr content  after doReturnAPtr is: {}\n", *returnedPtr);
+
+//     doSomethingStatic();
+//     doSomethingStatic();
+//     doSomethingStatic();
+//     doSomethingStatic();
+
+//     fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
+//     fmt::print("Value of foo2 {} address of foo2 {}\n", foo2, fmt::ptr(&foo2));
+//     fmt::print("Value of data {} address of data {}\n", data, fmt::ptr(&data));
+//     fmt::print("Value of data2 {} address of data2 {}\n", data2, fmt::ptr(&data2));
+//     fmt::print("Value of rodata {} address of rodata {}\n", rodata, fmt::ptr(&rodata));
+//     fmt::print("Value of rodata2 {} address of rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
+
+//     int var;
+//     static int var2;
+//     fmt::print("Value of var {} address of var {}\n", var, fmt::ptr(&var));
+//     fmt::print("Value of var2 {} address of var2 {}\n", var2, fmt::ptr(&var2));
+
+//     return 0; /* exit gracefully*/
+// }
 
 
 /*Aufgabe 1*/
@@ -93,3 +129,50 @@ auto main(int argc, char** argv) -> int
 Memory Leaks und Stack Overflows, da meist kein Betriebssystem und nur begrenzter Speicher vorhanden ist 
 --> Fehler können zu Systemabstürzen der seltsamen Verhalten*/
 
+/*Aufgabe 4*/
+/*1. Wann tritt ein Stack Overflow auf?
+
+Ein Stack Overflow passiert, wenn der Aufrufstapel (Stack) überläuft – also mehr Speicher benutzt wird, als für den Stack reserviert ist.
+Typische Ursachen:
+
+Zu tiefe Rekursion (eine Funktion ruft sich selbst zu oft auf)
+
+Sehr große lokale Variablen (z. B. große Arrays auf dem Stack)
+
+Endlosschleifen mit Funktionsaufrufen
+
+2. Warum entstehen Memory Leaks?
+
+Ein Memory Leak (Speicherleck) entsteht, wenn dynamisch (z. B. mit malloc() oder new) Speicher angefordert, aber nicht wieder freigegeben wird (free() bzw. delete fehlt).
+Dadurch bleibt der Speicher belegt, bis das Programm endet — auf Mikrocontrollern mit wenig RAM kann das schnell kritisch werden.
+
+3. Was ist gefährlicher: Ein Zombie-Pointer oder ein Leak?
+
+Ein Zombie-Pointer (Dangling Pointer) ist gefährlicher, weil er auf Speicher zeigt, der nicht mehr gültig ist – jede Nutzung kann zu unvorhersehbarem Verhalten oder Abstürzen führen.
+Ein Leak verschwendet zwar Speicher, verursacht aber nicht sofort falsches Verhalten.
+
+4. Wie kann man solche Fehler frühzeitig erkennen?
+
+Möglichkeiten:
+
+- Statische Analyse-Tools (z. B. clang-tidy, cppcheck)
+- Dynamische Debugging-Tools (z. B. valgrind, AddressSanitizer)
+- Stack-Guards und Watchdogs auf Mikrocontrollern
+- Saubere Programmierdisziplin (Initialisierung, Freigabe, NULL setzen)
+- Unit Tests und Code Reviews
+
+Diagramm des Speichers:
+-------------------------------------------------------------------
++-------------------------+  ← hohe Speicheradresse
+|      Stack              |  (lokale Variablen, Funktionsaufrufe)
+|      ↓ wächst nach unten|
++-------------------------+
+|      Heap               |  (malloc/new - dynamische Daten)
+|      ↑ wächst nach oben |
++-------------------------+
+|   Daten-Segment (.data) |  (globale & statische Variablen)
++-------------------------+
+|   Code-Segment (.text)  |  (Programmcode, Funktionen)
++-------------------------+  ← niedrige Speicheradresse
+
+*/
